ece58e1edd2d9115830d041163cbaef9772835e4
Sung-jae Park <nicesj.park@samsung.com>
2013-06-18 13:50:48 +0900

    Remove unnecessary storage space check code
    [model] Redwood
    [binary_type] AP
    [customer] Docomo/Orange/Open
    [issue#] P130607-6207
    [problem] PD open/close is not related with storage space.
    [cause] Even if a user tries to open a PD, master checks disk space for openning it.
    [solution] PD open is not necessary to check the free disk space. So remove it.
    [team] HomeTF
    [request]
    [horizontal_expansion]
    
    Change-Id: Ibaccf506dabaee5f32a9065a8472de41f336f052

--- src/server.c
+++ src/server.c
@@ -4711,9 +4711,7 @@ static struct packet *client_create_pd(pid_t pid, int handle, const struct packe
 	if (ret != LB_STATUS_SUCCESS)
 		goto out;
 
-	if (util_free_space(IMAGE_PATH) < MINIMUM_SPACE) {
-		ret = LB_STATUS_ERROR_NO_SPACE;
-	} else if (instance_pd_owner(inst)) {
+	if (instance_pd_owner(inst)) {
 		ret = LB_STATUS_ERROR_ALREADY;
 	} else if (package_pd_type(instance_package(inst)) == PD_TYPE_BUFFER) {
 		lazy_pd_destroyed_cb(inst);
@@ -5978,13 +5976,6 @@ static struct packet *slave_acquire_buffer(pid_t pid, int handle, const struct p
 		goto out;
 	}
 
-	if (util_free_space(IMAGE_PATH) < MINIMUM_SPACE) {
-		ErrPrint("Not enough space\n");
-		ret = LB_STATUS_ERROR_NO_SPACE;
-		id = "";
-		goto out;
-	}
-
 	ret = validate_request(pkgname, id, &inst, &pkg);
 	id = "";
 
@@ -6120,13 +6111,6 @@ static struct packet *slave_resize_buffer(pid_t pid, int handle, const struct pa
 	if (ret != 5) {
 		ErrPrint("Invalid argument\n");
 		ret = LB_STATUS_ERROR_INVALID;
-		id = "";
-		goto out;
-	}
-
-	if (util_free_space(IMAGE_PATH) < MINIMUM_SPACE) {
-		ErrPrint("Not enough space\n");
-		ret = LB_STATUS_ERROR_NO_SPACE;
 		id = "";
 		goto out;
 	}
