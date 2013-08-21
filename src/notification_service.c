62a179740aed741d760da0c0356fc022a6e5e7ba
youngsub ko <ys4610.ko@samsung.com>
2013-08-21 10:25:34 +0900

    adding notification setting service

--- src/notification_service.c
+++ src/notification_service.c
@@ -332,6 +332,69 @@ static void _handler_delete_multiple(struct tcb *tcb, struct packet *packet, voi
 	}
 }
 
+static void _handler_set_noti_property(struct tcb *tcb, struct packet *packet, void *data)
+{
+	int ret = 0, ret_p = 0;
+	struct packet *packet_reply = NULL;
+	char *pkgname = NULL;
+	char *property = NULL;
+	char *value = NULL;
+
+	if (packet_get(packet, "sss", &pkgname, &property, &value) == 3) {
+		pkgname = get_string(pkgname);
+		property = get_string(property);
+		value = get_string(value);
+
+		ret = notification_setting_db_set(pkgname, property, value);
+
+		packet_reply = packet_create_reply(packet, "ii", ret, ret);
+		if (packet_reply) {
+			if ((ret_p = service_common_unicast_packet(tcb, packet_reply)) < 0) {
+				ErrPrint("failed to send reply packet:%d\n", ret_p);
+			}
+			packet_destroy(packet_reply);
+		} else {
+			ErrPrint("failed to create a reply packet\n");
+		}
+
+		if (ret != NOTIFICATION_ERROR_NONE) {
+			ErrPrint("failed to set noti property:%d\n", ret);
+		}
+	} else {
+		ErrPrint("Failed to get data from the packet");
+	}
+}
+
+static void _handler_get_noti_property(struct tcb *tcb, struct packet *packet, void *data)
+{
+	int ret = 0, ret_p = 0;
+	struct packet *packet_reply = NULL;
+	char *pkgname = NULL;
+	char *property = NULL;
+	char *value = NULL;
+
+	if (packet_get(packet, "sss", &pkgname, &property) == 2) {
+		pkgname = get_string(pkgname);
+		property = get_string(property);
+
+		ret = notification_setting_db_get(pkgname, property, &value);
+
+		packet_reply = packet_create_reply(packet, "is", ret, value);
+		if (packet_reply) {
+			if ((ret_p = service_common_unicast_packet(tcb, packet_reply)) < 0) {
+				ErrPrint("failed to send reply packet:%d\n", ret_p);
+			}
+			packet_destroy(packet_reply);
+		} else {
+			ErrPrint("failed to create a reply packet\n");
+		}
+
+		if (value != NULL) {
+			DbgFree(value);
+		}
+	}
+}
+
 static void _handler_service_register(struct tcb *tcb, struct packet *packet, void *data)
 {
 	int ret = 0;
@@ -372,6 +435,22 @@ static void _handler_access_control_error_refresh(struct tcb *tcb, struct packet
 	struct packet *packet_reply = NULL;
 
 	packet_reply = packet_create_reply(packet, "i", NOTIFICATION_ERROR_PERMISSION_DENIED);
+	if (packet_reply) {
+		if ((ret_p = service_common_unicast_packet(tcb, packet_reply)) < 0) {
+			ErrPrint("Failed to send a reply packet:%d", ret_p);
+		}
+		packet_destroy(packet_reply);
+	} else {
+		ErrPrint("Failed to create a reply packet");
+	}
+}
+
+static void _handler_access_control_error_get_property(struct tcb *tcb, struct packet *packet)
+{
+	int ret_p = 0;
+	struct packet *packet_reply = NULL;
+
+	packet_reply = packet_create_reply(packet, "is", NOTIFICATION_ERROR_PERMISSION_DENIED, NULL);
 	if (packet_reply) {
 		if ((ret_p = service_common_unicast_packet(tcb, packet_reply)) < 0) {
 			ErrPrint("Failed to send a reply packet:%d", ret_p);
@@ -439,6 +518,20 @@ static int service_thread_main(struct tcb *tcb, struct packet *packet, void *dat
 			.rule = "data-provider-master::notification.client",
 			.access = "w",
 			.handler_access_error = _handler_access_control_error_common,
+		},
+		{
+			.cmd = "set_noti_property",
+			.handler = _handler_set_noti_property,
+			.rule = "data-provider-master::notification.client",
+			.access = "w",
+			.handler_access_error = _handler_access_control_error_common,
+		},
+		{
+			.cmd = "get_noti_property",
+			.handler = _handler_get_noti_property,
+			.rule = "data-provider-master::notification.client",
+			.access = "r",
+			.handler_access_error = _handler_access_control_error_get_property,
 		},
 		{
 			.cmd = "service_register",
