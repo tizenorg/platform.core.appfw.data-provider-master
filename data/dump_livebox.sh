b61e2ec3acbe8426aca826449ec0e192dcdf1112
Sung-jae Park <nicesj.park@samsung.com>
2013-12-18 15:25:57 +0900

    Create folder first before dump the logs.
    Change-Id: Idf68ecb76e6fe2a3f500f28ab9d6e75cef005b7d

--- data/dump_livebox.sh
+++ data/dump_livebox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 LIVEBOX_DEBUG=$1/livebox
-mkdir_function ${LIVEBOX_DEBUG}
+mkdir -p ${LIVEBOX_DEBUG}
 /bin/cp -r /opt/usr/share/live_magazine ${LIVEBOX_DEBUG}
 /bin/cp -r /tmp/.dbox.service ${LIVEBOX_DEBUG}/log
