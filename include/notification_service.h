acb6497416cd540bca8cbc1074ce0fc16266425b
Hyunho Kang <hhstark.kang@samsung.com>
2016-06-07 16:44:41 +0900

    Add multi-user feature
    Change-Id: I219d685577b341fe6a6fbcf0852593a65110884b
    Signed-off-by: Hyunho Kang <hhstark.kang@samsung.com>

--- include/notification_service.h
+++ include/notification_service.h
@@ -19,24 +19,23 @@
 extern int notification_service_init(void);
 extern int notification_service_fini(void);
 
-int notification_server_register(GVariant *parameters, GVariant **reply_body);
-int notification_add_noti(GVariant *parameters, GVariant **reply_body);
-int notification_update_noti(GVariant *parameters, GVariant **reply_body);
-int notification_refresh_noti(GVariant *parameters, GVariant **reply_body);
-int notification_del_noti_single(GVariant *parameters, GVariant **reply_body);
-int notification_del_noti_multiple(GVariant *parameters, GVariant **reply_body);
-int notification_set_noti_property(GVariant *parameters, GVariant **reply_body);
-int notification_get_noti_property(GVariant *parameters, GVariant **reply_body);
-int notification_get_noti_count(GVariant *parameters, GVariant **reply_body);
-int notification_update_noti_setting(GVariant *parameters, GVariant **reply_body);
-int notification_update_noti_sys_setting(GVariant *parameters, GVariant **reply_body);
-int notification_load_noti_by_tag(GVariant *parameters, GVariant **reply_body);
-int notification_load_noti_by_priv_id(GVariant *parameters, GVariant **reply_body);
-int notification_load_grouping_list(GVariant *parameters, GVariant **reply_body);
-int notification_load_detail_list(GVariant *parameters, GVariant **reply_body);
-int notification_get_setting_array(GVariant *parameters, GVariant **reply_body);
-int notification_get_setting_by_package_name(GVariant *parameters, GVariant **reply_body);
-int notification_load_system_setting(GVariant *parameters, GVariant **reply_body);
+int notification_add_noti(GVariant *parameters, GVariant **reply_body, GList *monitoring_list, uid_t uid);
+int notification_update_noti(GVariant *parameters, GVariant **reply_body, GList *monitoring_list, uid_t uid);
+int notification_refresh_noti(GVariant *parameters, GVariant **reply_body, GList *monitoring_list, uid_t uid);
+int notification_del_noti_single(GVariant *parameters, GVariant **reply_body, GList *monitoring_list, uid_t uid);
+int notification_del_noti_multiple(GVariant *parameters, GVariant **reply_body, GList *monitoring_list, uid_t uid);
+int notification_set_noti_property(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_get_noti_property(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_get_noti_count(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_update_noti_setting(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_update_noti_sys_setting(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_load_noti_by_tag(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_load_noti_by_priv_id(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_load_grouping_list(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_load_detail_list(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_get_setting_array(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_get_setting_by_package_name(GVariant *parameters, GVariant **reply_body, uid_t uid);
+int notification_load_system_setting(GVariant *parameters, GVariant **reply_body, uid_t uid);
 int notification_register_dbus_interface();
 
 /* End of a file */
